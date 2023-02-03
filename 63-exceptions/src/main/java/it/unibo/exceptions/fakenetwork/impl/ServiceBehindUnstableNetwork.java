09e3b0bb3d4491b22cb5ee07feabd66c7fb8a36a
vasile.dragus <vasile.dragus@studio.unibo.it>
2023-02-03 16:15:18 +0100

    added check in constructor

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,10 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if ( failProbability < 0 || failProbability >= 1) {
+            final String msg = "failProbability " + failProbability + "out of bounds, it should be in [0, 1[!";
+            throw new IllegalArgumentException(msg);
+        }
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
