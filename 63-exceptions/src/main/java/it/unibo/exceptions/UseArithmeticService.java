db96c7445cf948de3d78776e72489a8f4d7988db
vasile.dragus <vasile.dragus@studio.unibo.it>
2023-02-03 16:39:05 +0100

    added exception handling logic in first two methods

--- 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
@@ -3,6 +3,7 @@ package it.unibo.exceptions;
 import it.unibo.exceptions.fakenetwork.api.NetworkComponent;
 import it.unibo.exceptions.fakenetwork.impl.ServiceBehindUnstableNetwork;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static it.unibo.exceptions.arithmetic.ArithmeticService.DIVIDED;
@@ -46,14 +47,29 @@ public final class UseArithmeticService {
          * This method should re-try to send message to the provided server, catching all IOExceptions,
          * until it succeeds.
          */
-    }
+        while (true) {
+            try {
+                server.sendData(message);
+                return;
+            } catch (final IOException e) {
+                LOG.println(e.getMessage());
+            }
+        }
+
+        }
 
     private static String retryReceiveOnNetworkError(final NetworkComponent server) {
         /*
          * This method should re-try to retrieve information from the provided server, catching all IOExceptions,
          * until it succeeds.
          */
-        return null;
+        while (true) {
+            try {
+                return server.receiveResponse();
+            } catch (final IOException e) {
+                LOG.println(e.getMessage());
+            }
+        }
     }
 
     private static void assertEqualsAsDouble(final String expected, final String actual) {
