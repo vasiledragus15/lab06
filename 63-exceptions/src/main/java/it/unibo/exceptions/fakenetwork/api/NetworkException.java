c769b5f0fd6452800dac8617f052a2e328c27395
vasile.dragus <vasile.dragus@studio.unibo.it>
2023-02-03 16:54:14 +0100

    create NetworkException class

---
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/api/NetworkException.java
@@ -0,0 +1,17 @@
+package it.unibo.exceptions.fakenetwork.api;
+
+import java.io.IOException;
+
+public class NetworkException extends IOException {
+
+    private final String s;
+        public NetworkException() {
+            super();
+        }
+
+        public NetworkComponent (final String s ) {
+            super();
+            
+
+        }
+}
